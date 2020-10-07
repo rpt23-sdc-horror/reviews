f812a3884cce59aea67dbb17fec1f42c2fe9b8d2
Martin <martinsung00@gmail.com>
2020-10-06 17:47:59 -0700

    JSON package fixed to reflect reviews module. Collapsible reviews implementation.

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,3 +1,7 @@
+/* eslint-disable no-else-return */
+/* eslint-disable react/button-has-type */
+/* eslint-disable no-plusplus */
+/* eslint-disable max-len */
 /* eslint-disable react/jsx-one-expression-per-line */
 /* eslint-disable react/no-unused-state */
 /* eslint-disable react/destructuring-assignment */
@@ -5,7 +9,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import $ from 'jquery';
-import Reviews from './components/Reviews.jsx';
+// import Reviews from './components/Reviews.jsx';
 import './style.css';
 
 class ReviewsModule extends React.Component {
@@ -18,9 +22,16 @@ class ReviewsModule extends React.Component {
       averageSize: [],
       averageComfort: [],
       averageDurability: [],
+      collapsed: true,
     };
+    this.toggleCollapsed = this.toggleCollapsed.bind(this);
   }
 
+  toggleCollapsed() {
+    this.setState({
+      collapsed: !this.collapsed,
+    });
+  }
   // componentDidMount() {
   //   $.ajax({
   //     url: 'localhost:3003/reviews/:productID',
@@ -36,10 +47,59 @@ class ReviewsModule extends React.Component {
   // }
 
   render() {
+    let numberOfReviews = 0;
+
+    if (this.state.reviews === 0 || this.state.reviews === undefined) {
+      numberOfReviews = 0;
+    } else {
+      numberOfReviews = this.state.reviews.length;
+    }
+
     return (
       <div>
-        <h1>Item List</h1>
-        <Reviews reviews={this.state.reviews} />
+        <button
+          style={{ width: '600px' }}
+          className="collapsible"
+          onClick={() => {
+            const coll = document.getElementsByClassName('collapsible');
+            let i;
+
+            for (i = 0; i < coll.length; i++) {
+              coll[i].addEventListener('click', function () {
+                this.classList.toggle('active');
+                const content = this.nextElementSibling;
+                if (content.style.maxHeight) {
+                  content.style.maxHeight = null;
+                } else {
+                  content.style.maxHeight = `${content.scrollHeight}px`;
+                }
+              });
+            }
+            this.collapsed();
+          }}
+        >Reviews ({numberOfReviews})
+          <div style={{ float: 'right' }}>
+            <div className="rating">
+              <div className="rating-upper" style={{ width: '90%' }}>
+                <span>★</span>
+                <span>★</span>
+                <span>★</span>
+                <span>★</span>
+                <span>★</span>
+              </div>
+              <div className="rating-lower">
+                <span>★</span>
+                <span>★</span>
+                <span>★</span>
+                <span>★</span>
+                <span>★</span>
+              </div>
+            </div>
+          </div>
+        </button>
+        <div className="content" style={{ width: '564px' }}>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
       </div>
     );
   }
