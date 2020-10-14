c534283c1be41cbdc709086cf005d87ecb00a5b1
Martin <martinsung00@gmail.com>
2020-10-14 16:57:29 -0700

    Add modal component to react-components

---
+++ react-client/src/components/Modal.jsx
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import '../style.css';
+
+// const Modal = (props) => (
+//   <div className="modal">
+//     Hello
+//   </div>
+// );
+
+// export default Modal;
